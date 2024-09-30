12f7d5e7267ec1a62f8c7a46976d27d5490e5a81
DatSung <datsung.dev@gmail.com>
2024-09-30 14:11:01 +0700

    feat: define collection api calling

--- solara-admin-frontend/src/utils/axios/collectionApi.ts
+++ solara-admin-frontend/src/utils/axios/collectionApi.ts
@@ -1,12 +1,15 @@
-import {BaseReModel, PageReqModel, PaginationResModel, SubResModel} from "../../types/general.type.ts";
-import {CollectionModel} from "../../types/collection.type.ts";
+import {BaseModel, BaseReModel, PageReqModel, PaginationResModel, SubResModel} from "../../types/general.type.ts";
+import {CollectionModel, CollectionResModel, UpsertCollectionReqModel} from "../../types/collection.type.ts";
 import axiosClient from "./axiosClient.ts";
 import {COLLECTION_URL} from "../url/collectionUrl.ts";
 
 interface ICollectionApi {
     getCollections: (query: PageReqModel) => Promise<BaseReModel<SubResModel<PaginationResModel<CollectionModel>>>>
     getCollection: (id: string) => Promise<BaseReModel<CollectionModel>>;
-    getOnTopic: (id: string) => Promise<BaseReModel<SubResModel<PaginationResModel<CollectionModel>>>>
+    getOnTopic: (id: string) => Promise<BaseReModel<SubResModel<PaginationResModel<CollectionModel>>>>;
+    postCollection: (request: UpsertCollectionReqModel) => Promise<BaseReModel<CollectionResModel>>;
+    putCollection: (request: UpsertCollectionReqModel) => Promise<BaseReModel<CollectionResModel>>;
+    deleteCollection: (id: string) => Promise<BaseModel>;
 }
 
 export const collectionApi: ICollectionApi = {
@@ -15,11 +18,23 @@ export const collectionApi: ICollectionApi = {
         return response.data;
     },
     getCollection: async (id: string) => {
-        const response = await axiosClient.get(COLLECTION_URL.GET_ON_TOPIC(id));
+        const response = await axiosClient.get<BaseReModel<CollectionModel>>(COLLECTION_URL.GET_ON_TOPIC(id));
         return response.data;
     },
     getOnTopic: async (id: string) => {
-        const response = await axiosClient.get(COLLECTION_URL.GET_ON_TOPIC(id));
+        const response = await axiosClient.get<BaseReModel<SubResModel<PaginationResModel<CollectionModel>>>>(COLLECTION_URL.GET_ON_TOPIC(id));
+        return response.data;
+    },
+    postCollection: async (request: UpsertCollectionReqModel) => {
+        const response = await axiosClient.post<BaseReModel<CollectionResModel>>(COLLECTION_URL.GET_POS_PUT_DEL(request));
+        return response.data;
+    },
+    putCollection: async (request: UpsertCollectionReqModel) => {
+        const response = await axiosClient.put<BaseReModel<CollectionResModel>>(COLLECTION_URL.GET_POS_PUT_DEL(request));
+        return response.data;
+    },
+    deleteCollection: async (id: string) => {
+        const response = await axiosClient.get<BaseModel>(COLLECTION_URL.GET_POS_PUT_DEL(id));
         return response.data;
     }
 }
